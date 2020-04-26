4f05200893255f22821d631d719471ec4ca9436f
DGIII <34903375+DomGonzalesIII@users.noreply.github.com>
2020-04-25 22:06:11 -0500

    Completed Project
    Clicks and keydowns trigger sound and animation.

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
 /* jshint esversion: 6 */
 
+// for each .drum class button, add a click event listener
+// upon a .drum button being clicked, play drum sound and animate button
 var drums = document.querySelectorAll('.drum');
 
 drums.forEach((drum, i) => {
-  drum.addEventListener('click', handleClick);
+  drum.addEventListener('click', function () {
+    soundOff(this.innerHTML);
+    buttonAnimation(this.innerHTML);
+  });
 });
 
-function handleClick() {
-  alert("I've been clicked!");
+// for the entire document, add a keydown event listener
+// upon a key press that corresponds to a drum, play drum sound and animate button
+document.addEventListener('keydown', function (event) {
+  soundOff(event.key);
+  buttonAnimation(event.key);
+});
+
+// determines which specific drum sound to play
+function soundOff(key) {
+
+  switch (key) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+    default: console.log(key);
+
+  }
+}
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector(`.${currentKey}`);
+
+  activeButton.classList.add('pressed');
+  setTimeout(function () {activeButton.classList.remove('pressed');}, 200);
 }
